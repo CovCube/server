ff1d2d40bde8a26b3e12811856e1d4789a268712
ArcticFall <23174635+ArcticFall@users.noreply.github.com>
2021-07-06 15:43:51 +0200

    Add api stubs.

--- src/api/cubes.ts
+++ src/api/cubes.ts
@@ -2,3 +2,23 @@ import express, { Router } from "express";
 
 //Export the router
 export var router: Router = express.Router();
+
+router.get('/', function(req, res) {
+    res.send(200);
+});
+
+router.post('/', function(req, res) {
+    res.send(200);
+});
+
+router.get('/:cubeId', function(req, res) {
+    res.send(200);
+});
+
+router.put('/:cubeId', function(req, res) {
+    res.send(200);
+});
+
+router.delete('/:cubeId', function(req, res) {
+    res.send(200);
+});
--- src/api/data.ts
+++ src/api/data.ts
@@ -2,3 +2,7 @@ import express, { Router } from "express";
 
 //Export the router
 export var router: Router = express.Router();
+
+router.get('/', function(req, res) {
+    res.send(200);
+});
