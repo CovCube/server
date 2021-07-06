8845bb70dd151a9b3796ef45300e9895c2e3b278
ArcticFall <23174635+ArcticFall@users.noreply.github.com>
2021-07-06 16:41:42 +0200

    Add comments to index.

--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,20 @@ import helmet from "helmet";
 import dotenv from "dotenv";
 import {router as apiRoutes} from "./api/api";
 
+//Parse environment variables
 dotenv.config();
-
+//Create express app
 const PORT = process.env.PORT || 3000;
 const app: Express = express();
 //const index = require('./routes/index')
 
+//Add middleware
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//Delegate routing
 app.get("/", apiRoutes);
 
+//Start server
 app.listen(PORT, () => console.log(`Running on port: ${PORT}`));
