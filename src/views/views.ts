b6dce9f2fddcdd47d0fc805860abc23ce2515562
ArcticFall <23174635+ArcticFall@users.noreply.github.com>
2021-07-27 14:24:05 +0200

    Add cube list view.

--- src/views/views.ts
+++ src/views/views.ts
@@ -1,5 +1,22 @@
 import express, { Router, Request, Response } from "express";
-import path from 'path';
+import { getCubes } from "../utils/db_utils";
+import { Cube } from "../types";
 
 //Export the router
 export var router: Router = express.Router();
+
+//Delegate view-routes to their views
+router.get('/', (req, res) => {
+    getCubes()
+        .then((cubes: Array<Cube>) => {
+            let data = {
+                cubes: cubes,
+            }
+
+            res.render('index', data);
+        })
+        .catch ((e: Error) => {
+            console.log(e.stack);
+            res.status(501).send("database error");
+        });
+});
