9b973ce83b03928c22b5ae5352a42532c8d28d13
ArcticFall <23174635+ArcticFall@users.noreply.github.com>
2021-08-05 12:47:02 +0200

    Move addSensorActuator function outside DOMContentLoaded

--- public/js/cube-detail.js
+++ public/js/cube-detail.js
@@ -5,27 +5,28 @@ window.addEventListener('DOMContentLoaded', () => {
     document.getElementById("add-sensor-button").addEventListener('click', addSensorActuator.bind(this, "sensor"));
     document.getElementById("add-actuator-button").addEventListener('click', addSensorActuator.bind(this, "actuator"));
     
-    function addSensorActuator (type) {
-
-        let select_elem = document.getElementById('additional_'+type+'s_select');
-        let option = select_elem.selectedOptions.item(0).value;
-
-        //Add row and cells
-        let table = document.getElementById(type+'s');
-        let new_row = table.insertRow(table.rows.length-1);
-        let name_cell = new_row.insertCell();
-        let delete_cell = new_row.insertCell();
-        //Populate cells
-        name_cell.innerHTML = option;
-        delete_cell.innerHTML = deleteSensorActuatorHTML;
-
-        //Remove option from select
-        let option_elem = document.getElementById('add-'+type+'-'+option);
-        option_elem.parentNode.removeChild(option_elem);
-
-        //Remove select if empty
-        if (select_elem.children.length == 0) {
-            document.getElementById('additional_'+type+'s_row').style.display= 'none';
-        }
-    }
 });
+
+function addSensorActuator (type) {
+
+    let select_elem = document.getElementById('additional_'+type+'s_select');
+    let option = select_elem.selectedOptions.item(0).value;
+
+    //Add row and cells
+    let table = document.getElementById(type+'s');
+    let new_row = table.insertRow(table.rows.length-1);
+    let name_cell = new_row.insertCell();
+    let delete_cell = new_row.insertCell();
+    //Populate cells
+    name_cell.innerHTML = option;
+    delete_cell.innerHTML = deleteSensorActuatorHTML;
+
+    //Remove option from select
+    let option_elem = document.getElementById('add-'+type+'-'+option);
+    option_elem.parentNode.removeChild(option_elem);
+
+    //Remove select if empty
+    if (select_elem.children.length == 0) {
+        document.getElementById('additional_'+type+'s_row').style.display= 'none';
+    }
+}
