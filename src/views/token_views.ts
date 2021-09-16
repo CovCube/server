4ca9994529081fe6a471c85de5e012b7a5a4573d
ArcticFall <23174635+ArcticFall@users.noreply.github.com>
2021-09-16 17:07:37 +0200

    Add ability to add tokens in view.

--- src/views/token_views.ts
+++ src/views/token_views.ts
@@ -5,7 +5,7 @@ import { Token } from "../types";
 import express from "express";
 //internal imports
 import { authenticateUser } from "../utils/passport_utils";
-import { getTokens, getTokenByToken, deleteToken } from "../utils/db_token_utils";
+import { getTokens, getTokenByToken, deleteToken, addToken } from "../utils/db_token_utils";
 
 export var router: Router = express.Router();
 
@@ -15,6 +15,19 @@ router.get('/', (req: Request, res: Response) => {
     getTokens()
         .then((tokens) => {
             res.render("tokens-list", {tokens: tokens});
+        })
+        .catch((e: Error) => {
+            console.log(e.stack);
+            res.status(501).send("view error");
+        });
+});
+
+router.post('/', (req: Request, res: Response) => {
+    let owner: string = req.body['owner'];
+
+    addToken(owner)
+        .then(() => {
+            res.redirect(303, '/tokens');
         })
         .catch((e: Error) => {
             console.log(e.stack);
