eee0f8739e9071a29194045254d4d56720b7306f
ArcticFall <23174635+ArcticFall@users.noreply.github.com>
2021-09-16 16:14:44 +0200

    Add view for tokens.

--- src/views/views.ts
+++ src/views/views.ts
@@ -5,6 +5,7 @@ import express from "express";
 //internal imports
 import { router as cubeViewsRouter } from "./cubes_views";
 import { router as userViewsRouter } from "./user_views";
+import { router as tokenViewsRouter } from "./token_views";
 import { router as passportViewsRouter } from "./passport_views";
 
 //Export the router
@@ -15,3 +16,4 @@ router.use('/', passportViewsRouter)
 router.get('/', (req: Request, res: Response) => {res.redirect(303, '/cubes')})
 router.use('/cubes', cubeViewsRouter);
 router.use('/users', userViewsRouter);
+router.use('/tokens', tokenViewsRouter);
