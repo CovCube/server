502dd4b1386f2d9b687cc2d7af443b4bc3d2f7bb
ArcticFall <23174635+ArcticFall@users.noreply.github.com>
2021-09-29 10:47:26 +0200

    Add ability to get sensor data via the api.

--- src/api/data.ts
+++ src/api/data.ts
@@ -2,10 +2,20 @@
 import { Router, Request, Response } from "express";
 //express imports
 import express from "express";
+import { getSensorData } from "../model/sensor_data";
 
 //Export the router
 export var router: Router = express.Router();
 
 router.get('/', function(req: Request, res: Response) {
-    res.send(200);
+    getSensorData()
+        .then((sensor_data) => {
+            res.status(200).send({
+                "sensor_data": sensor_data
+            });
+        })
+        .catch((e: Error) => {
+            console.log(e.stack);
+            res.status(501).send("database error");
+        });
 });
--- src/model/sensor_data.ts
+++ src/model/sensor_data.ts
@@ -1,4 +1,5 @@
 //internal imports
+import { QueryResult } from "pg";
 import { pool } from "../index";
 import { getTimestamp } from "../utils/general_utils";
 import { checkCubeId } from "../utils/input_check_utils";
@@ -6,6 +7,9 @@ import { checkCubeId } from "../utils/input_check_utils";
 //sensor data tables
 const createNumericDataTableQuery: string = "CREATE TABLE IF NOT EXISTS numeric_data (id SERIAL PRIMARY KEY, sensor_type CHAR(64) NOT NULL, cube_id UUID NOT NULL, timestamp TIMESTAMPTZ NOT NULL, data NUMERIC NOT NULL, FOREIGN KEY(cube_id) REFERENCES cubes(id) ON DELETE CASCADE)";
 const createAlphanumericDataTableQuery: string = "CREATE TABLE IF NOT EXISTS alphanumeric_data (id SERIAL PRIMARY KEY, sensor_type CHAR(64) NOT NULL, cube_id UUID NOT NULL, timestamp TIMESTAMPTZ NOT NULL, data CHAR(64) NOT NULL, FOREIGN KEY(cube_id) REFERENCES cubes(id) ON DELETE CASCADE)";
+//Get sensor data
+const getNumericDataQuery: string = "SELECT * FROM numeric_data";
+const getAlphanumericDataQuery: string = "SELECT * FROM alphanumeric_data";
 //Persist sensor data
 const persistNumericDataQuery: string = "INSERT INTO numeric_data (sensor_type, cube_id, timestamp, data) VALUES ($1, $2, $3, $4)";
 const persistAlphanumericDataQuery: string = "INSERT INTO alphanumeric_data (sensor_type, cube_id, timestamp, data) VALUES ($1, $2, $3, $4)";
@@ -20,6 +24,39 @@ export function createSensorDataTable(): Promise<void> {
         } catch(err) {
             return reject(err);
         }
+    });
+}
+
+export function getSensorData(): Promise<Array<Object>> {
+    return new Promise(async (resolve, reject) => {
+
+        let res_rows;
+        try {
+            let numeric_res: QueryResult = await pool.query(getNumericDataQuery);
+            let alphanumeric_res: QueryResult = await pool.query(getAlphanumericDataQuery);
+
+            res_rows = numeric_res.rows.concat(alphanumeric_res.rows);
+        } catch(err) {
+            return reject(err);
+        }
+
+        let sensor_data: Array<Object> = [];
+
+        res_rows.forEach((row) => {
+
+            if (typeof row.data == "string") {
+                row.data = row.data.trim();
+            }
+
+            sensor_data.push({
+                "sensorType": row.sensor_type.trim(),
+                "cubeId": row.cube_id,
+                "timestamp": row.timestamp,
+                "data": row.data,
+            });
+        });
+
+        return resolve(sensor_data);
     });
 }
 
