81d64aaaa7474975af6a64d0d9917952708d614b
ArcticFall <23174635+ArcticFall@users.noreply.github.com>
2021-09-29 10:23:51 +0200

    Also change SQL queries.

--- src/model/sensor_data.ts
+++ src/model/sensor_data.ts
@@ -4,11 +4,11 @@ import { getTimestamp } from "../utils/general_utils";
 import { checkCubeId } from "../utils/input_check_utils";
 
 //sensor data tables
-const createNumericDataTableQuery: string = "CREATE TABLE IF NOT EXISTS sensor_data (id SERIAL PRIMARY KEY, sensor_type CHAR(64) NOT NULL, cube_id UUID NOT NULL, timestamp TIMESTAMPTZ NOT NULL, data NUMERIC NOT NULL, FOREIGN KEY(cube_id) REFERENCES cubes(id) ON DELETE CASCADE)";
-const createAlphanumericDataTableQuery: string = "CREATE TABLE IF NOT EXISTS nfc_data (id SERIAL PRIMARY KEY, sensor_type CHAR(64) NOT NULL, cube_id UUID NOT NULL, timestamp TIMESTAMPTZ NOT NULL, data CHAR(64) NOT NULL, FOREIGN KEY(cube_id) REFERENCES cubes(id) ON DELETE CASCADE)";
+const createNumericDataTableQuery: string = "CREATE TABLE IF NOT EXISTS numeric_data (id SERIAL PRIMARY KEY, sensor_type CHAR(64) NOT NULL, cube_id UUID NOT NULL, timestamp TIMESTAMPTZ NOT NULL, data NUMERIC NOT NULL, FOREIGN KEY(cube_id) REFERENCES cubes(id) ON DELETE CASCADE)";
+const createAlphanumericDataTableQuery: string = "CREATE TABLE IF NOT EXISTS alphanumeric_data (id SERIAL PRIMARY KEY, sensor_type CHAR(64) NOT NULL, cube_id UUID NOT NULL, timestamp TIMESTAMPTZ NOT NULL, data CHAR(64) NOT NULL, FOREIGN KEY(cube_id) REFERENCES cubes(id) ON DELETE CASCADE)";
 //Persist sensor data
-const persistNumericDataQuery: string = "INSERT INTO sensor_data (sensor_type, cube_id, timestamp, data) VALUES ($1, $2, $3, $4)";
-const persistAlphanumericDataQuery: string = "INSERT INTO nfc_data (sensor_type, cube_id, timestamp, data) VALUES ($1, $2, $3, $4)";
+const persistNumericDataQuery: string = "INSERT INTO numeric_data (sensor_type, cube_id, timestamp, data) VALUES ($1, $2, $3, $4)";
+const persistAlphanumericDataQuery: string = "INSERT INTO alphanumeric_data (sensor_type, cube_id, timestamp, data) VALUES ($1, $2, $3, $4)";
 
 export function createSensorDataTable(): Promise<void> {
     return new Promise(async (resolve, reject) => {
