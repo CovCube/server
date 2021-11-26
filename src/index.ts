43518856964d537951b49f20bccd0670f19df752
ArcticFall <23174635+ArcticFall@users.noreply.github.com>
2021-11-26 13:56:13 +0100

    Add ability to change the CSP during runtime.
    This is in preparation for including app iframes,
    whose addresses need to be added to the allowed domains in CSP.

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@
 
 // Type imports
 import { Express } from "express";
+import { App } from "./types";
 // External imports
 import express from "express";
 import hbs from 'hbs';
@@ -20,7 +21,7 @@ import passport from "passport";
 import { nav_apps } from "./views/views";
 import { createCubeTables } from "./model/cube";
 import { createSensorDataTable } from "./model/sensor_data";
-import { createAppsTable } from "./model/app";
+import { createAppsTable, getAvailableApps } from "./model/app";
 import { createUserTable } from "./model/user";
 import { createTokensTable } from "./model/token";
 import { setupPassport } from "./utils/passport_utils";
@@ -51,8 +52,14 @@ hbs.registerHelper('nav_apps', nav_apps);
 // Register static path
 app.use('/static', express.static(path.join(__dirname, './public')));
 
-// Add middleware
-app.use(helmet());
+// Add helmet middleware
+// CSP middleware needs to be axtra, so it can be replaced later
+app.use(helmet({
+    contentSecurityPolicy: false,
+}));
+app.use(helmet.contentSecurityPolicy());
+
+// Add other middleware
 app.use(session({
     secret: process.env.SESSIONSECRET || 'secret',
     // Check if session store implements touch
@@ -116,4 +123,58 @@ async function setupServer(): Promise<void> {
     } catch(err) {
         console.log(err);
     }
+}
+
+export function updateHelmetCSP() {
+    
+    // Get apps
+    let apps: Array<App> = getAvailableApps();
+    // Define standard directives
+    let frame_src: Array<string> = ["'self'"];
+    let script_src: Array<string> = ["'self'"];
+    let style_src: Array<string> = ["'self'", "https: 'unsafe-inline'"];
+
+    // Add custom directives
+    apps.forEach((app: App) => {
+        if (app.address !== '') {
+            frame_src.push(app.address);
+            script_src.push(app.address);
+            style_src.push(app.address);
+        }
+    });
+
+    // Remove existing helmet middleware from stack
+    let stack: Array<any> = app._router.stack;
+    let helmetIndex: number = stack.length + 1;
+
+    stack.forEach((layer: any, index: number) => {
+        if (!layer.name) {
+            return;
+        }
+
+        if (layer.name === 'contentSecurityPolicyMiddleware') {
+            helmetIndex = index;
+        }
+    });
+
+    if (helmetIndex <= stack.length) {
+        stack.splice(helmetIndex, 1);
+        app._router.stack = stack;
+    }
+
+    // Add new helmet middleware
+    app.use(helmet.contentSecurityPolicy({
+        useDefaults: true,
+        directives: {
+            "frame-src": frame_src,
+            "script-src": script_src,
+            "style-src": style_src
+        }
+    }));
+
+    // Move middleware before routers
+    stack = app._router.stack;
+    let middleware = stack.pop();
+    stack.splice(helmetIndex, 0, middleware);
+    app._router.stack = stack;
 }
